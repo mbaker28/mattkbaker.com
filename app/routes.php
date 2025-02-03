39674aac2670e12269edc0ff2a5254b913002efe
Matthew Baker <mattbakerk@gmail.com>
2025-02-03 01:02:30 -0600

    Updated design - Removed Resume page - redundant to resume download - Removed Markdown Twig extension - Added experience page - Added summary and skills to home page - Added technologies used list under experience and projects blocks - Moved contact links to footer - Added resume download link to floating container - Updated composer.lock file

--- app/routes.php
+++ app/routes.php
@@ -6,10 +6,11 @@ use App\Controller\ProjectController;
 use App\Controller\HomeController;
 use App\Controller\HelloWorldController;
 use App\Controller\ContactController;
+use App\Controller\ExperienceController;
 
 return FastRoute\simpleDispatcher(function (RouteCollector $r) {
 	$r->addRoute('GET', '/', HomeController::class);
 	$r->addRoute('GET', '/projects', ProjectController::class);
-	$r->addRoute('GET', '/resume', ResumeController::class);
 	$r->addRoute('GET', '/contact', ContactController::class);
+	$r->addRoute('GET', '/experience', ExperienceController::class);
 });
--- src/Controller/ExperienceController.php
+++ src/Controller/ExperienceController.php
@@ -7,13 +7,13 @@ namespace App\Controller;
 use Twig\Environment;
 use Symfony\Component\HttpFoundation\Response;
 
-final class ResumeController extends AbstractController
+final class ExperienceController extends AbstractController
 {
 	public function __invoke(): Response
 	{
-		return $this->render('resume.html.twig', [
-			'title' => 'Resume',
-			'title_link' => '/resume'
+		return $this->render('experience.html.twig', [
+			'title' => 'Experience',
+			'title_link' => '/experience'
 		]);
 	}
 }
