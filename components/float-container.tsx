6ff48835913a62aa0b5257d55917612266be4dcd
Matthew Baker <mattbakerk@gmail.com>
2025-02-14 14:14:19 -0600

    Updated resume

--- components/float-container.tsx
+++ components/float-container.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from "next/link";
 import ThemeSwitcher from "./theme-switcher";
 
 export default function FloatContainer() {
@@ -8,7 +9,7 @@ export default function FloatContainer() {
 			className="flex flex-row bottom-8 right-8 z-100 fixed text-[1.6em]"
 		>
 			<div className="flex mr-8">
-				<a
+				<Link
 					id="download-resume"
 					href="/resume.docx"
 					className="group text-8 opacity-50 text-[color:var(--alt-fg-color)] bg-[color:var(--alt-bg-color)] hover:text-[color:var(--alt-fg-color)] focus:text-[color:var(--alt-fg-color)] hover:opacity-50 focus:opacity-50 md:hover:text-[color:var(--link-color)] md:focus:text-[color:var(--link-color)] md:hover:opacity-100 md:focus:opacity-100"
@@ -19,7 +20,7 @@ export default function FloatContainer() {
 						>
 							Download Resume
 					</span>
-				</a>
+				</Link>
 			</div>
 			<ThemeSwitcher />
 		</div>
