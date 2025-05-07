7e626f13457422e3a6aee041800dc49bf2267661
Matt Baker <mattbakerk@gmail.com>
2025-05-07 11:18:02 -0500

    Update data.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -349,7 +349,7 @@ export const projects: Project[] = [
 	{
 		title: 'URL Shortener',
 		href: 'https://github.com/mbaker28/url-shortener',
-		description: `A simple URL Shortener app written with <a href="https://nextjs.org" class="no-underline hover:text-[color:var(--link-color)]" target="_blank">Next.js</a> as the frontend and <a href="https://symfony.com" class="no-underline hover:text-[color:var(--link-color)]" target="_blank">Symfony</a> as the backend.`,
+		description: `A simple URL Shortener app written with <a href="https://symfony.com" class="no-underline hover:text-[color:var(--link-color)]" target="_blank">Symfony</a> and <a href="https://react.dev" class="no-underline hover:text-[color:var(--link-color)]" target="_blank">React</a>. <a href="https://shortener.mattkbaker.com" class="no-underline hover:text-[color:var(--link-color)]" target="_blank">Click here</a> to demo the app.`,
 		image: {
 			src: '/url-shortener-thumb.png',
 			alt: 'URL Shortener',
@@ -361,7 +361,6 @@ export const projects: Project[] = [
 			'JavaScript',
 			'TypeScript',
 			'React',
-			'Next.js',
 			'Tailwind CSS',
 			'Symfony',
 			'MySQL'
